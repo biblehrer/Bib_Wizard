b3bfdc40a0a02f4f8230bfecd33ba5e093536d34
Stefan Gerstner <stefan.gerstner96@web.de>
2024-04-16 16:13:09 +0200

    Fireball

--- Assets/Introduction/Scripts/Fireball.cs
+++ Assets/Introduction/Scripts/Fireball.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Fireball : MonoBehaviour
 {
+    public Vector3 direction; 
+
     // Start is called before the first frame update
     void Start()
     {
+        //float angle = Vector3.Angle(direction, Vector3.right);
+
+        if (direction.y > 0)
+        {
+            transform.Rotate(new Vector3(0,0,90));
+        }
+        if (direction.y < 0)
+        {
+            transform.Rotate(new Vector3(0,0,270));
+        }
+        if (direction.x < 0)
+        {
+            transform.Rotate(new Vector3(0,0,180));
+        }
+
         Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0, 4,0) * Time.deltaTime;        
+        transform.position = transform.position + direction *4 * Time.deltaTime;        
     }
 }
--- Assets/Introduction/Scripts/Wizard.cs
+++ Assets/Introduction/Scripts/Wizard.cs
@@ -7,6 +7,7 @@ public class Wizard : MonoBehaviour
     public GameObject fireballPrefab;
     float castTimer = 0f;
     float movementSpeed = 2.0f;
+    Vector3 lastMovement = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -39,14 +40,20 @@ public class Wizard : MonoBehaviour
         {
             movement += Vector3.right;
         } 
-               
+
+        if (movement.x > 0 || movement.y > 0 || movement.x < 0 || movement.y < 0 )
+        {
+            lastMovement = movement;
+        }
+
+
         float sprintSpeedFactor = 1.0f;
         if (Input.GetKey(KeyCode.LeftShift))
         {
             sprintSpeedFactor = 2.0f;
         }
 
-        transform.position += movement.normalized * Time.deltaTime * movementSpeed * sprintSpeedFactor;   
+        transform.position += movement.normalized * Time.deltaTime * movementSpeed * sprintSpeedFactor;  
 
         //
         // Casting
@@ -55,8 +62,9 @@ public class Wizard : MonoBehaviour
         castTimer -= Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space) && castTimer <= 0)
         {
-            Instantiate(fireballPrefab, transform.position, Quaternion.identity);
-            castTimer = 3;
+            GameObject obj = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+            obj.GetComponent<Fireball>().direction = lastMovement;
+            castTimer = 1;
         }     
         
     }
